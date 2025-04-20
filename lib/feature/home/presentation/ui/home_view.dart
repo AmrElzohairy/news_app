5c44c4e15f2ca4c161a2007c1fb326e7d83d0567
amrElzohairy <aelzohairy9@gmail.com>
2025-04-20 15:18:09 +0200

    add tab bar

--- lib/feature/home/presentation/ui/home_view.dart
+++ lib/feature/home/presentation/ui/home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:news_app/core/utils/app_colors.dart';
+import 'package:news_app/core/utils/app_styles.dart';
 import 'package:news_app/feature/home/presentation/ui/widgets/home_header.dart';
 import 'package:news_app/feature/home/presentation/ui/widgets/rounded_carousal_slider.dart';
 
@@ -7,14 +10,35 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            const SizedBox(height: 20),
-            HomeHeader(),
-            RoundedCarouselSlider(),
-          ],
+    return DefaultTabController(
+      length: 4,
+      initialIndex: 0,
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              HomeHeader(),
+              const SizedBox(height: 30),
+              RoundedCarouselSlider(),
+              const SizedBox(height: 30),
+              TabBar(
+                indicatorColor: AppColors.primaryColor,
+                indicatorWeight: 3,
+                indicatorSize: TabBarIndicatorSize.label,
+                labelColor: Colors.white,
+                unselectedLabelColor: Colors.white,
+                labelStyle: AppStyles.font12BoldWhite,
+                labelPadding: EdgeInsets.symmetric(horizontal: 5.w),
+                tabs: const [
+                  Tab(text: 'Science'),
+                  Tab(text: 'Environment'),
+                  Tab(text: 'Animals'),
+                  Tab(text: 'Travel'),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
