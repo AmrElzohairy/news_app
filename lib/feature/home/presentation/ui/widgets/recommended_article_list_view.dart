0bec936f1776cc06351af4ed2f3a07abeb550248
amrElzohairy <aelzohairy9@gmail.com>
2025-04-20 17:08:43 +0200

    change physics

--- lib/feature/home/presentation/ui/widgets/recommended_article_list_view.dart
+++ lib/feature/home/presentation/ui/widgets/recommended_article_list_view.dart
@@ -2,30 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:news_app/feature/home/presentation/ui/widgets/recommended_article_item.dart';
 import 'package:news_app/feature/home/presentation/ui/widgets/recommended_article_widget.dart';
+import 'package:news_app/feature/home/presentation/ui/widgets/vertical_artical_item.dart';
 
 class RecommendedArticleListView extends StatelessWidget {
   const RecommendedArticleListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        RecommendedArticleWidget(),
-        const SizedBox(height: 20),
-        SizedBox(
-          height: 245.h,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: 5,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12),
-                child: RecommendedArticleItem(),
-              );
-            },
+    return SingleChildScrollView(
+      physics: const NeverScrollableScrollPhysics(),
+      child: Column(
+        children: [
+          RecommendedArticleWidget(),
+          const SizedBox(height: 20),
+          SizedBox(
+            height: 245.h,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  child: RecommendedArticleItem(),
+                );
+              },
+            ),
           ),
-        ),
-      ],
+          const SizedBox(height: 20),
+          RecommendedArticleWidget(),
+          const SizedBox(height: 20),
+          VerticalArticalItem(),
+        ],
+      ),
     );
   }
 }
---
+++ lib/feature/home/presentation/ui/widgets/vertical_artical_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VerticalArticalItem extends StatelessWidget {
+  const VerticalArticalItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
