d74c2ef8d38b58783a45060da329ba919b54b0b7
amrElzohairy <aelzohairy9@gmail.com>
2025-04-20 14:05:28 +0200

    add app styles

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -5,4 +5,6 @@ class AppColors {
   static const Color scaffoldColor = Color(0xFF13140D);
   static const Color black = Color(0xFF000000);
   static const Color white = Color(0xFFFFFFFF);
+  static const Color textGrey = Color(0xFF858585);
+  static const Color lightGrey = Color(0xFFD9DAD3);
 }
--- lib/core/utils/app_styles.dart
+++ lib/core/utils/app_styles.dart
@@ -1,3 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:news_app/core/utils/app_colors.dart';
+
 class AppStyles {
-  
+  static TextStyle font18SemiBoldWhite = TextStyle(
+    color: AppColors.white,
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle font9SemiBoldWhite = TextStyle(
+    color: AppColors.white,
+    fontSize: 9.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle font14BoldWhite = TextStyle(
+    color: AppColors.white,
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle font12BoldWhite = TextStyle(
+    color: AppColors.white,
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle font24BoldWhite = TextStyle(
+    color: AppColors.white,
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle font12BoldPrimary = TextStyle(
+    color: AppColors.primaryColor,
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle font10RegularWhite = TextStyle(
+    color: AppColors.white,
+    fontSize: 10.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle font8RegularPrimary = TextStyle(
+    color: AppColors.primaryColor,
+    fontSize: 8.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle font12RegularGrey = TextStyle(
+    color: AppColors.textGrey,
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle font12RegularLightGrey = TextStyle(
+    color: AppColors.lightGrey,
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w400,
+  );
 }
